2b091eac11314a0f89fdd1ecea7fdd06878b92ab
superstark01 <superstark02@gmail.com>
2021-08-14 21:22:17 +0530

    12/12

--- src/Pages/BucketView.js
+++ src/Pages/BucketView.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import ReactStickyNotes from '@react-latest-ui/react-sticky-notes';
-import { Link } from 'react-router-dom';
+import Draggable from 'react-draggable'; // The default
 import "../App.css"
+import { Link } from 'react-router-dom';
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { rdb } from "../firebase"
 
-class Home extends Component {
+class BucketView extends Component {
 
     state = {
-        notes: null,
+        notes: [],
         disablePan: true,
         x: 0,
         y: 0,
         scale: null,
-        buckets: [],
     }
 
     toggleZoom = () => {
@@ -22,52 +21,6 @@ class Home extends Component {
         } else {
             this.setState({ disablePan: true })
         }
-    }
-
-    onChange = (type, payload, notes) => {
-
-        if(type==="add"){
-            notes[0]["bucket"] = "Bucket Name";
-            notes[0]["highlight"] = "Highlight";
-            notes[0].text = "Bucket Name" + " : "  + "Highlight";
-        }
-
-        if(type === "update"){
-            for(var i = 0; i < notes.length; i++){
-                if(payload.data.id === notes[i].id){
-                    var temp = notes[i].text.split(" : ");
-                    notes[i]["bucket"] = temp[0];
-                    notes[i]["highlight"] = temp[1];
-                }
-            }
-        }
-
-        for (var i = 0; i < notes.length; i++) {
-            for (var j = 0; j < notes.length; j++) {
-                if (notes[i].position.x - notes[j].position.x < 218 && notes[i].position.x - notes[j].position.x > 0) {
-                    notes[i].bucket = notes[j].bucket;
-                    var temp = notes[i].text.split(" : ");
-                    notes[i].text = notes[j].bucket + " : " + temp[1]
-                }
-                else if (notes[i].position.x - notes[j].position.x < 218 && notes[i].position.x - notes[j].position.x > 0) {
-                    notes[j].bucket = notes[i].bucket;
-                    var temp = notes[j].text.split(" : ");
-                    notes[j].text = notes[i].bucket + " : " + temp[1]
-                }
-            }
-        }
-
-        this.setState({
-            notes
-        })
-
-        if(notes.length !== 0){
-            rdb.ref().child("notes").set(notes)
-        }
-
-        console.log(type)
-        console.log(payload)
-        console.log(notes)
     }
 
     onPanStop = (e) => {
@@ -92,10 +45,93 @@ class Home extends Component {
         })
     }
 
+
+    addNote = () => {
+
+        var d = new Date();
+        var n = d.getTime();
+
+        var arr = []
+
+        arr = this.state.notes
+
+        arr.push({
+            id: n,
+            bucket: "New Bucket",
+            highlight: "New Highlight",
+            x: 0,
+            y: 0
+        })
+        rdb.ref().child("notes").set(arr)
+        this.setState({ notes: arr })
+    }
+
+    deletNote = (id) => {
+        var i
+        for (i = 0; i < this.state.notes.length; i++) {
+            if (id == this.state.notes[i].id) {
+                break;
+            }
+        }
+        var arr = this.state.notes
+        arr.splice(i, 1)
+
+        this.setState({ notes: arr })
+        rdb.ref().child("notes").set(arr)
+    }
+
+    editNote = (id, type, e) => {
+        var i
+        for (i = 0; i < this.state.notes.length; i++) {
+            if (id == this.state.notes[i].id) {
+                break;
+            }
+        }
+        var arr = this.state.notes
+
+        if (type === "bucket") {
+            arr[i].bucket = e.target.value
+        }
+        if (type === "highlight") {
+            arr[i].highlight = e.target.value
+        }
+        this.setState({ notes: arr })
+
+        rdb.ref().child("notes").set(arr)
+    }
+
+    updateBucket = (e, id) => {
+        var i
+        for (i = 0; i < this.state.notes.length; i++) {
+            if (id == this.state.notes[i].id) {
+                break;
+            }
+        }
+        var arr = this.state.notes
+
+        arr[i].x = e.x
+        arr[i].y = e.y
+        rdb.ref().child("notes").child(i).update({ x: e.x, y: e.y })
+
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < arr.length; j++) {
+                if (arr[i].x - arr[j].x < 233 && arr[i].x - arr[j].x > 0) {
+                    arr[i].bucket = arr[j].bucket;
+                }
+                else if (arr[j].x - arr[i].x < 233 && arr[j].x - arr[i].x > 0) {
+                    arr[j].bucket = arr[i].bucket;
+                }
+            }
+        }
+
+        console.log(e)
+
+        this.setState({ notes: arr })
+    }
+
     componentDidMount() {
         rdb.ref().child("notes").on('value', (snapshot) => {
-            this.setState({ notes: snapshot.val() })
-            console.log(this.state.notes)
+           this.setState({ notes: snapshot.val() })
         });
 
         rdb.ref().child("PanZoom").on('value', (snapshot) => {
@@ -106,7 +142,7 @@ class Home extends Component {
     }
 
     render() {
-        if (this.state.notes && this.state.scale) {
+        if (true) {
             return (
                 <TransformWrapper
                     initialScale={this.state.scale}
@@ -122,14 +158,17 @@ class Home extends Component {
                     onZoomStop={this.onZoomStop}
                 >
                     {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-                        <React.Fragment>
+                        <div>
                             <div className="app-bar" >
                                 <div>
                                     Affinty Map
                                 </div>
                                 <div className="btn" >
+                                    <div variant="contain" className="btns" onClick={this.addNote} >
+                                        Add
+                                    </div>
                                     <div variant="contain" className="btns" onClick={this.toggleZoom} >
-                                        Zoom And Pan
+                                        Zoom On
                                     </div>
                                     <Link >
                                         <div className="btns">
@@ -152,40 +191,42 @@ class Home extends Component {
                                 </div>
                             </div>
 
+
                             <TransformComponent>
                                 <div style={{ minHeight: "90vh", width: "100vw" }} >
-                                    <ReactStickyNotes
-                                        notes={this.state.notes}
-                                        onChange={this.onChange}
-                                        footer={true}
-                                    />
-                                    <div className="bucket-ctn" >
-                                        {
-                                            this.state.buckets.map(item => {
-                                                return (
-                                                    <div className="bucket" >
-                                                        <p className="bucket-name" >{item.title}</p>
+                                    {
+                                        this.state.notes.map(item => {
+                                            return (
+                                                <Draggable
+                                                    onStop={(e) => { this.updateBucket(e, item.id) }}
+                                                >
+                                                    <div style={{ backgroundColor: "#cef7f7", width: "200px", height: "200px" }} >
                                                         <div>
-                                                            <img onClick={()=>{this.deleteBucket(item.id)}} style={{ marginRight: "10px", cursor: "pointer" }} src="https://img.icons8.com/material-outlined/24/000000/delete-sign.png" width="10px" />
-                                                            <img style={{ cursor: "pointer" }} src="https://img.icons8.com/material-outlined/24/000000/pencil--v1.png" width="10px" />
+                                                            <input value={item.bucket} onChange={(e) => { this.editNote(item.id, "bucket", e) }} />
                                                         </div>
+                                                        <div>
+                                                            <input value={item.highlight} onChange={(e) => { this.editNote(item.id, "highlight", e) }} />
+                                                        </div>
+                                                        <button onClick={() => { this.deletNote(item.id) }} >
+                                                            Delete
+                                                        </button>
                                                     </div>
-                                                )
-                                            })
-                                        }
-                                    </div>
+                                                </Draggable>
+                                            )
+                                        })
+                                    }
                                 </div>
                             </TransformComponent>
-                        </React.Fragment>
+                        </div>
                     )}
 
                 </TransformWrapper>
             );
-        }else{
+        }
+        else{
             return(<div>Loading</div>)
         }
     }
 }
 
-export default Home;
-
+export default BucketView;
---
+++ src/Pages/Group.js
@@ -0,0 +1,274 @@
+import React, { Component } from 'react';
+import Draggable from 'react-draggable'; // The default
+import "../App.css"
+import { Link } from 'react-router-dom';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { rdb } from "../firebase"
+
+class Group extends Component {
+
+    state = {
+        notes: [],
+        disablePan: true,
+        x: 0,
+        y: 0,
+        scale: null,
+        buckets: []
+    }
+
+    toggleZoom = () => {
+        if (this.state.disablePan) {
+            this.setState({ disablePan: false })
+        } else {
+            this.setState({ disablePan: true })
+        }
+    }
+
+    onPanStop = (e) => {
+        this.setState({ x: e.state.positionX })
+        this.setState({ y: e.state.positionY })
+        this.setState({ scale: e.state.scale })
+        rdb.ref().child("PanZoom").set({
+            x: e.state.positionX,
+            y: e.state.positionY,
+            scale: e.state.scale
+        })
+    }
+
+    onZoomStop = (e) => {
+        this.setState({ x: e.state.positionX })
+        this.setState({ y: e.state.positionY })
+        this.setState({ scale: e.state.scale })
+        rdb.ref().child("PanZoom").set({
+            x: e.state.positionX,
+            y: e.state.positionY,
+            scale: e.state.scale
+        })
+    }
+
+
+    addNote = () => {
+
+        var d = new Date();
+        var n = d.getTime();
+
+        var arr = []
+
+        arr = this.state.notes
+
+        arr.push({
+            id: n,
+            bucket: "New Bucket",
+            highlight: "New Highlight",
+            x: 0,
+            y: 0
+        })
+        rdb.ref().child("notes").set(arr)
+        this.setState({ notes: arr })
+    }
+
+    deletNote = (id) => {
+        var i
+        for (i = 0; i < this.state.notes.length; i++) {
+            if (id == this.state.notes[i].id) {
+                break;
+            }
+        }
+        var arr = this.state.notes
+        arr.splice(i, 1)
+
+        this.setState({ notes: arr })
+        rdb.ref().child("notes").set(arr)
+    }
+
+    editNote = (id, type, e) => {
+        var i
+        for (i = 0; i < this.state.notes.length; i++) {
+            if (id == this.state.notes[i].id) {
+                break;
+            }
+        }
+        var arr = this.state.notes
+
+        if (type === "bucket") {
+            arr[i].bucket = e.target.value
+        }
+        if (type === "highlight") {
+            arr[i].highlight = e.target.value
+        }
+        this.setState({ notes: arr })
+
+        rdb.ref().child("notes").set(arr)
+    }
+
+    updateBucket = (e, id) => {
+        var i
+        for (i = 0; i < this.state.notes.length; i++) {
+            if (id == this.state.notes[i].id) {
+                break;
+            }
+        }
+        var arr = this.state.notes
+
+        arr[i].x = e.x
+        arr[i].y = e.y
+        rdb.ref().child("notes").child(i).update({ x: e.x, y: e.y })
+
+        for (var i = 0; i < arr.length; i++) {
+            for (var j = 0; j < arr.length; j++) {
+                if (arr[i].x - arr[j].x < 233 && arr[i].x - arr[j].x > 0) {
+                    arr[i].bucket = arr[j].bucket;
+                }
+                else if (arr[j].x - arr[i].x < 233 && arr[j].x - arr[i].x > 0) {
+                    arr[j].bucket = arr[i].bucket;
+                }
+            }
+        }
+
+        console.log(e)
+
+        this.setState({ notes: arr })
+    }
+
+    componentDidMount() {
+        rdb.ref().child("notes").on('value', (snapshot) => {
+            this.setState({ notes: snapshot.val() })
+
+            var arr = []
+            var buckets = []
+            arr = snapshot.val()
+
+            for (var i = 0; i < arr.length; i++) {
+                var b = -1;
+
+                for (var k = 0; k < buckets.length; k++) {
+                    if (arr[i].bucket === buckets[k].id) {
+                        b = k;
+                        break;
+                    }
+                }
+
+                if (b !== -1) {
+                    buckets[b].highlights.push(arr[i])
+                }
+                else {
+                    buckets.push({
+                        id: arr[i].bucket,
+                        highlights: [arr[i]]
+                    })
+                }
+            }
+
+            this.setState({ buckets: buckets })
+            console.log(buckets)
+        });
+
+        rdb.ref().child("PanZoom").on('value', (snapshot) => {
+            this.setState({ x: snapshot.val().x })
+            this.setState({ y: snapshot.val().y })
+            this.setState({ scale: snapshot.val().scale })
+        });
+    }
+
+    render() {
+        if (true) {
+            return (
+                <TransformWrapper
+                    initialScale={this.state.scale}
+                    initialPositionX={this.state.x}
+                    initialPositionY={this.state.y}
+                    disabled={this.state.disablePan}
+                    doubleClick={
+                        {
+                            disabled: true
+                        }
+                    }
+                    onPanningStop={this.onPanStop}
+                    onZoomStop={this.onZoomStop}
+                >
+                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                        <div>
+                            <div className="app-bar" >
+                                <div>
+                                    Affinty Map
+                                </div>
+                                <div className="btn" >
+                                    <div variant="contain" className="btns" onClick={this.addNote} >
+                                        Add
+                                    </div>
+                                    <div variant="contain" className="btns" onClick={this.toggleZoom} >
+                                        Zoom On
+                                    </div>
+                                    <Link >
+                                        <div className="btns">
+                                            Group Highlights
+                                        </div>
+                                    </Link>
+                                    <a href="https://github.com/superstark02/affiliate-map" >
+                                        <div className="btns-i" >
+                                            <img title="Go To Source Code" src="https://img.icons8.com/material-outlined/24/000000/github.png" width="15px" />
+                                        </div>
+                                    </a>
+                                    <a href="https://drive.google.com/file/d/1YFMy3DLDIFezlxw42Cqgj4A9AgCiiL1L/view?usp=sharing" >
+                                        <div className="btns-i" >
+                                            <img title="Go To Video" src="https://img.icons8.com/material-outlined/24/000000/video.png" width="15px" />
+                                        </div>
+                                    </a>
+                                    <div className="btns-i" >
+                                        <img title="Open Info" onClick={() => { this.setState({ openInfo: true }) }} src="https://img.icons8.com/material-outlined/24/000000/info.png" width="15px" />
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <TransformComponent>
+                                <React.Fragment>
+                                    <div style={{ minHeight: "90vh", width: "100vw" }} >
+                                        {
+                                            this.state.notes.map(item => {
+                                                return (
+                                                    <Draggable>
+                                                        <div style={{ backgroundColor: "#cef7f7", width: "200px", height: "200px" }} >
+                                                            <div>
+                                                                <input value={item.bucket} onChange={(e) => { this.editNote(item.id, "bucket", e) }} />
+                                                            </div>
+                                                            <div>
+                                                                <input value={item.highlight} onChange={(e) => { this.editNote(item.id, "highlight", e) }} />
+                                                            </div>
+                                                            <button onClick={() => { this.deletNote(item.id) }} >
+                                                                Delete
+                                                            </button>
+                                                        </div>
+                                                    </Draggable>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <div className="bucket-ctn" >
+                                        {
+                                            this.state.buckets.map(item => {
+                                                return (
+                                                    <div className="bucket" >
+                                                        <p className="bucket-name" >
+                                                            {item.id}
+                                                        </p>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </React.Fragment>
+                            </TransformComponent>
+                        </div>
+                    )}
+
+                </TransformWrapper>
+            );
+        }
+        else {
+            return (<div>Loading</div>)
+        }
+    }
+}
+
+export default Group;
